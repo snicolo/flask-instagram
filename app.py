1c3212ee76eafbcf7a8185f7182043fcfb693345
snicolo <seby.nicol@gmail.com>
2018-12-17 12:43:19 +0100

    added followings list and fix to method get and post

--- app.py
+++ app.py
@@ -18,13 +18,15 @@ def igtag():
     if request.method == 'GET':
        username = request.args.get('user')
        password = request.args.get('pass')
+       tag = request.args.get('tag')
     else:
         username = request.values.get('users')
         password = request.values.get('passws')
+        tag = request.values.get('tags')
     
     InstagramAPI = InstagramAPI(username, password)
     InstagramAPI.login()
-    tag_search= request.args.get('tag')
+    tag_search= tag
     InstagramAPI.tagFeed(tag_search)
     time.sleep(1)
     result = InstagramAPI.LastJson
@@ -53,13 +55,16 @@ def igloc():
     if request.method == 'GET':
        username = request.args.get('user')
        password = request.args.get('pass')
+       location = request.args.get('loc')
+       locid = request.args.get('locid') 
     else:
         username = request.values.get('users')
         password = request.values.get('passws')
+        location = request.values.get('locs')
+        locid = request.values.get('locids')
     InstagramAPI = InstagramAPI(username, password)
     InstagramAPI.login()
-    location = request.args.get('loc')
-    locid = request.args.get('locid') 
+    
     #search by name
     place= InstagramAPI.searchLocation(location)
     InstagramAPI.getLocationFeed(place)
@@ -86,13 +91,15 @@ def igfollower():
     if request.method == 'GET':
        username = request.args.get('user')
        password = request.args.get('pass')
+       usertofind = request.args.get('usertofind')
     else:
         username = request.values.get('users')
         password = request.values.get('passws')
+        usertofind = request.values.get('usertofinds')
     
     InstagramAPI = InstagramAPI(username, password)
     InstagramAPI.login()
-    user_name = request.args.get('username')
+    user_name = usertofind
     InstagramAPI.searchUsername(user_name)
     username_id = InstagramAPI.LastJson["user"]["pk"]
 
@@ -110,6 +117,40 @@ def igfollower():
     followers_list=followers
     
     return jsonify(followers_list)
+
+
+@app.route('/igfollowing', methods=['GET', 'POST'])
+def igfollowing():
+    from InstagramAPI import InstagramAPI
+    if request.method == 'GET':
+       username = request.args.get('user')
+       password = request.args.get('pass')
+       usertofind = request.args.get('usertofind')
+    else:
+        username = request.values.get('users')
+        password = request.values.get('passws')
+        usertofind = request.values.get('usertofind')
+    
+    InstagramAPI = InstagramAPI(username, password)
+    InstagramAPI.login()
+    user_name = usertofind
+    InstagramAPI.searchUsername(user_name)
+    username_id = InstagramAPI.LastJson["user"]["pk"]
+
+    followings   = []
+    next_max_id = True
+    while next_max_id:
+     print next_max_id
+ 
+     if next_max_id == True: next_max_id=''
+     _ = InstagramAPI.getUserFollowings(username_id,maxid=next_max_id)
+     followings.extend ( InstagramAPI.LastJson.get('users',[]))
+     next_max_id = InstagramAPI.LastJson.get('next_max_id','')
+     time.sleep(1)
+    
+    followings_list=followings
+    
+    return jsonify(followings_list)
 
 
 
